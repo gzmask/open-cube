7ed8bf42bb70f2ffb73fb20b45e5ce2c536d821d
Ray Lei <gzmask@hotmail.com>
2010-03-19 14:55:23 -0600

    c to c plus plus

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 # Linux (default)
-EXE = test0001
+EXE = test0002
 LDFLAGS = -lGL -lGLU -lglut
 
 # Windows (cygwin)
 ifeq ($(OS), "Windows_NT")
-	EXE = test0001.exe
+	EXE = test0002.exe
 		LDFLAGS = -lopengl32 -lglu32 -lglut32
 endif
 
@@ -13,7 +13,7 @@ ifeq "$(shell uname)" "Darwin"
 	LDFLAGS = -framework Carbon -framework OpenGL -framework GLUT
 endif
 
-$(EXE) : src/test/test0001.c
-	gcc -o ./bin/$@ $< $(CFLAGS) $(LDFLAGS)
+$(EXE) : src/test/test0002.cpp
+	g++ -o ./bin/$@ $< $(CFLAGS) $(LDFLAGS)
 
 
---
+++ src/test/test0002.cpp
@@ -0,0 +1,63 @@
+/* GOAL:
+	1. A camera to view the scene;
+	2. A control schema to control the camera;
+	3. change this project from C to C++ ==done
+
+*/
+
+#ifdef __APPLE__
+#  	include <OpenGL/gl.h>
+#  	include <OpenGL/glu.h>
+#  	include <GLUT/glut.h>
+#else
+#  	include <GL/gl.h>
+#  	include <GL/glu.h>
+#  	include <GL/glut.h>
+#endif
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+	glColor3f (0.0, 0.0, 1.0);
+	glBegin(GL_QUADS);
+	glVertex2f(-0.75f, -0.75f);
+	glVertex2f( 0.75f, -0.75f);
+	glVertex2f( 0.75f,  0.75f);
+	glVertex2f(-0.75f,  0.75f);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+void reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void idle(void)
+{
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+
+	glutCreateWindow("GLUT Program");
+
+	glClearColor(1.0, 0.0, 0.0, 0.0); 
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
+
+	glutMainLoop();
+	return 0;
+}
+
+
+/*error message
+*/
